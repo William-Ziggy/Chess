27fdb420c225568f4e24b8af4b9e173939017ca1
William Emanuelsson <william.z.emanuelsson@gmail.com>
2019-07-02 21:59:26 +0200

    Lagt upp grundstrukturen.

---
+++ pieces.py
@@ -0,0 +1,21 @@
+class Piece:
+
+    def __init__():
+
+
+
+
+
+
+
+class King(Piece):
+
+class Queen(Piece):
+
+class Rook(Piece):
+
+class Bishop(Piece):
+
+class Knight(Piece):
+
+class Pawns(Piece):
