2726bcd8bf48c78ab321a594ee6812ec47afe06a
William Emanuelsson <william.z.emanuelsson@gmail.comxcode-select>
2020-01-29 17:46:13 +0100

    Change to JavaScript

---
+++ myscript.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+  /*
+  for(i=0; i<=8; i++){
+    for(j=0; j<=8; j++){
+      var square = document.createElement("div");
+      square.style.width = "50px";
+      square.style.height = "50px";
+      square.style.background = "#555";
+      square.style.position = "relative";
+      square.style.left = "100px";
+
+      document.getElementById("board").appendChild(square);
+    }
+  }
+}
