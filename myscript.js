552bc824bb50426feb8c15386ef9d7b47d065791
William Emanuelsson <william.z.emanuelsson@gmail.comxcode-select>
2020-02-05 20:09:38 +0100

    More script

--- myscript.js
+++ myscript.js
@@ -1,15 +1,24 @@
 window.onload=function(){
-  /*
-  for(i=0; i<=8; i++){
-    for(j=0; j<=8; j++){
-      var square = document.createElement("div");
-      square.style.width = "50px";
-      square.style.height = "50px";
-      square.style.background = "#555";
-      square.style.position = "relative";
-      square.style.left = "100px";
 
+  for(i=0; i<8; i++){
+    if(i%2==0){
+      white=1;
+    }else{
+      white=0;
+    }
+    for(j=0; j<8; j++){
+      var square = document.createElement("div");
       document.getElementById("board").appendChild(square);
+      square.style.width = "12.5%";
+      square.style.height = "12.5%";
+      if (white==0){
+        square.style.background = "black";
+        white=1;
+      }else {
+        square.style.background = "white";
+        white=0;
+      }
+      square.style.display = "inline-block";
     }
   }
 }
